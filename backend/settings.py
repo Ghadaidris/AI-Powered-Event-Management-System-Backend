8275c2f69436b9c0087bd4087faddafe3281eeac
GhadaIdris <ghada33264@gmail.com>
2025-10-30 10:06:59 +0300

    completed 3 models (UserProfile, Company, Event) with full CRUD + JWT authentication

--- backend/settings.py
+++ backend/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',  # cancels the html and renders the json to test the backend 
+    ),
+}
